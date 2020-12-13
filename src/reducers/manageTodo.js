0f29ede4f647449d90c1bf9ceaecbb5ac1fc1a5c
no0ro <nrolwes@gmail.com>
2020-12-13 09:54:37 -0500

    done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,13 +1,38 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
     case 'ADD_TODO':
 
-      return { todos: state.todos.concat(action.payload.text) };
+      const todo = {
+        id: Math.random()*10000000000000000,
+        text: action.payload.text
+      }
+      return { todos: state.todos.concat(todo) };
+
+    case 'DELETE_TODO':
+
+      return {todos: state.todos.filter(todo => todo.id !== action.payload)}
 
     default:
       return state;
   }
 }
+
+
+
+
+// export default function manageTodo(state = {
+// --   todos: [],
+// }, action) {
+//   console.log(action)
+//   switch (action.type) {
+//     case 'ADD_TODO':
+
+//       return { todos: state.todos.concat(action.payload.text) };
+
+//     default:
+//       return state;
+//   }
+// }
